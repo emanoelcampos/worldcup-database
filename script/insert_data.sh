7332e30825574c9bccaeeedb41c95915be2a2140
emanoelcampos <emanoel.cv@gmail.com>
2024-03-21 23:10:13 -0300

    reformate code

--- script/insert_data.sh
+++ script/insert_data.sh
@@ -1,7 +1,6 @@
 #! /bin/bash
 
-if [[ $1 == "test" ]]
-then
+if [[ $1 == "test" ]]; then
   PSQL="psql --username=postgres --dbname=worldcuptest -t --no-align -c"
 else
   PSQL="psql --username=freecodecamp --dbname=worldcup -t --no-align -c"
@@ -9,30 +8,24 @@ fi
 
 # Do not change code above this line. Use the PSQL variable above to query your database.
 
-cat games.csv | while IFS="," read YEAR ROUND WINNER OPPONENT WINNER_GOALS OPPONENT_GOALS
-do
-  if [[ $YEAR != "year" ]]
-  then
+cat games.csv | while IFS="," read YEAR ROUND WINNER OPPONENT WINNER_GOALS OPPONENT_GOALS; do
+  if [[ $YEAR != "year" ]]; then
     #get teams
     WINNER_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$WINNER'")
-    if [[ -z $WINNER_ID ]]
-    then
+    if [[ -z $WINNER_ID ]]; then
       # insert team
       INSERT_TEAM_RESULT=$($PSQL "INSERT INTO teams(name) VALUES('$WINNER')")
-      if [[ $INSERT_TEAM_RESULT == "INSERT 0 1" ]]
-      then
+      if [[ $INSERT_TEAM_RESULT == "INSERT 0 1" ]]; then
         echo "Inserted into teams, $WINNER"
       fi
-    # get new team id
-    WINNER_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$WINNER'")
+      # get new team id
+      WINNER_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$WINNER'")
     fi
 
     OPPONENT_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$OPPONENT'")
-    if [[ -z $OPPONENT_ID ]]
-    then
+    if [[ -z $OPPONENT_ID ]]; then
       INSERT_TEAM_RESULT_B=$($PSQL "INSERT INTO teams(name) VALUES('$OPPONENT')")
-      if [[ $INSERT_TEAM_RESULT_B == "INSERT 0 1" ]]
-      then
+      if [[ $INSERT_TEAM_RESULT_B == "INSERT 0 1" ]]; then
         echo "Inserted into teams, $OPPONENT"
       fi
       OPPONENT_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$OPPONENT'")
@@ -40,26 +33,21 @@ do
   fi
 done
 
-cat games.csv | while IFS="," read YEAR ROUND WINNER OPPONENT WINNER_GOALS OPPONENT_GOALS
-do
-  if [[ $YEAR != "year" ]]
-  then
+cat games.csv | while IFS="," read YEAR ROUND WINNER OPPONENT WINNER_GOALS OPPONENT_GOALS; do
+  if [[ $YEAR != "year" ]]; then
     WINNER_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$WINNER'")
-    if [[ -z $WINNER_ID ]]
-    then
-      WINNER_ID=null;
+    if [[ -z $WINNER_ID ]]; then
+      WINNER_ID=null
     fi
 
     OPPONENT_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$OPPONENT'")
-    if [[ -z $OPPONENT_ID ]]
-    then
-      OPPONENT_ID=null;
+    if [[ -z $OPPONENT_ID ]]; then
+      OPPONENT_ID=null
     fi
 
     # get game id
     INSERT_GAME_RESULT=$($PSQL "INSERT INTO games(year, round, winner_id, opponent_id, winner_goals, opponent_goals) VALUES($YEAR, '$ROUND', $WINNER_ID, $OPPONENT_ID, $WINNER_GOALS, $OPPONENT_GOALS)")
-    if [[ $INSERT_GAME_RESULT == "INSERT 0 1" ]]
-    then
+    if [[ $INSERT_GAME_RESULT == "INSERT 0 1" ]]; then
       echo "Inserted into games, $YEAR, $ROUND"
     fi
   fi
